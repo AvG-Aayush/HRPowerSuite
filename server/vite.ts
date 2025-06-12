05bfb6dd0934b139a6da7af9ad3f403949d68778
AvG-Aayush <avg.gaming.np@gmail.com>
2025-06-12 19:31:31 +0545

    save

--- server/vite.ts
+++ server/vite.ts
@@ -68,7 +68,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(import.meta.dirname, "public");
+  const distPath = path.resolve(__dirname, '..', '..', 'dist', 'public');
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -76,9 +76,6 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
-
-  // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
